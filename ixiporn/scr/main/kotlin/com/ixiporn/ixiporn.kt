7eb854947dbe9329b86a94334e914bd5423e649e
PartyTyme87 <prodege2@yahoo.com>
2026-08-12 05:50:03 +0300

    Update ixiporn.kt

--- ixiporn/scr/main/kotlin/com/ixiporn/ixiporn.kt
+++ ixiporn/scr/main/kotlin/com/ixiporn/ixiporn.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
 class ixiporn : MainAPI() {
-    override var mainUrl              = "https://ixiporn.org"
+    override var mainUrl              = "https://ixiporn.live"
     override var name                 = "ixiporn"
     override val hasMainPage          = true
     override var lang                 = "hi"
