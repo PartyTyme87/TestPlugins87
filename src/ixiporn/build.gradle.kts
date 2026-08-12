16212fa59dd6da71a90f7c92b23f010d09b1b97e
PartyTyme87 <prodege2@yahoo.com>
2026-08-12 08:10:27 +0300

    Create build.gradle.kts

---
+++ src/ixiporn/build.gradle.kts
@@ -0,0 +1,9 @@
+cloudstream {
+    // This must match your exact Plugin class name
+    setPlugin("com.PartyTyme87.ixiporn.IxipornPlugin") 
+    name = "Ixiporn"
+    description = "My custom extension"
+    authors.add("PartyTyme87")
+    versionName = "1.0.0"
+    versionCode = 1
+}
